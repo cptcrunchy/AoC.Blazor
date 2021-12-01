9e19664be694abacc641ab745a53435bc255e5f9
Jason Gutierrez <jasong84@gmail.com>
2021-11-30 21:46:43 -0600

    Init project

---
+++ Models/Puzzle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AoC.Blazor.Models
+{
+	public class Puzzle
+	{
+		public Puzzle()
+		{
+			
+		}
+		public Puzzle(string name, int day, string input)
+		{
+			Name = name;
+			Day = day;
+			Input = input;
+		}
+		public string Name { get; set; }
+		public int Day { get; set; }
+
+		public string Input { get; set; }
+
+	}
+}
---
+++ Services/Utilities.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using AoC.Blazor.Models;
+
+namespace AoC.Blazor.Services
+{
+	public class Utilities
+	{
+
+		public static async Task CreatePuzzleFile(Puzzle puzzle)
+		{
+			var puzzleDirectory = $"{Directory.GetCurrentDirectory()}\\Data\\";
+			await using var createPuzzleStream = File.Create($"{puzzleDirectory}{puzzle.Name}.json");
+			await JsonSerializer.SerializeAsync(createPuzzleStream, puzzle);
+			await createPuzzleStream.DisposeAsync();
+		}
+
+		public static Puzzle GetPuzzleInput(string puzzleName)
+		{
+			var puzzleDirectory = $"{Directory.GetCurrentDirectory()}\\Data\\";
+			if (!CheckPuzzleExists(puzzleName)) return new Puzzle();
+			var puzzleJson = File.ReadAllText($"{puzzleDirectory}{puzzleName}.json");
+			var puzzle = JsonSerializer.Deserialize<Puzzle>(puzzleJson);
+			return puzzle ?? new Puzzle();
+		}
+
+		public static bool CheckPuzzleExists(string puzzleName)
+		{
+			var puzzleDirectory = $"{Directory.GetCurrentDirectory()}\\Data\\";
+			return File.Exists($"{puzzleDirectory}{puzzleName}.json");
+		}
+
+		public static bool CheckDayPageExists(int number)
+		{
+			var daysDirectory = $"{Directory.GetCurrentDirectory()}\\Pages\\Days\\";
+			return File.Exists($"{daysDirectory}Day{number}.razor");
+		}
+	}
+}
