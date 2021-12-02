ba30c561a0472152d6cc5ed6cadc2a1d45367ef5
Jason Gutierrez <jasong84@gmail.com>
2021-12-01 23:57:23 -0600

    Improve the UI and UI feedback when creating puzzle input json files

--- Models/Puzzle.cs
+++ Models/Puzzle.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace AoC.Blazor.Models
+﻿namespace AoC.Blazor.Models
 {
 	public class Puzzle
 	{
