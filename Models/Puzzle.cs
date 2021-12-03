2ab97df15d760e11fa961e6dafe3c9ea9c820ce9
Jason Gutierrez <jasong84@gmail.com>
2021-12-03 11:13:30 -0600

    Update puzzle model to have practice property

--- Models/Puzzle.cs
+++ Models/Puzzle.cs
@@ -6,18 +6,20 @@
 		{
 			
 		}
-		public Puzzle(string name, int day, string input)
+		public Puzzle(string name, int day, bool isPractice, string input)
 		{
 			Name = name;
 			Day = day;
+			IsPractice = isPractice;
 			Input = input;
 		}
 		public string Name { get; set; }
 		public int Day { get; set; }
 		public string SolutionA { get; set; }
 		public string SolutionB { get; set; }
-
+		public bool IsPractice { get; set; }
 		public string Input { get; set; }
+		
 
 	}
 }
