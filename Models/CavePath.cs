bfd52ee24a4b6113678fd22d2f2338687216314d
Jason Gutierrez <jasong84@gmail.com>
2021-12-17 11:22:00 -0600

    Solve Day 15 Part 1

---
+++ Models/CavePath.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Threading.Tasks;
+
+namespace AoC.Blazor.Models
+{
+	public class CavePath
+	{
+		public (int x, int y) LastPosition { get; private set; }
+		public int Risk { get; private set; }
+
+		public CavePath((int x, int y) lastPosition)
+		{
+			LastPosition = lastPosition;
+			Risk = 0;
+		}
+
+		public CavePath(CavePath currentPosition, (int x, int y) lastPosition, int addRisk)
+		{
+			LastPosition = lastPosition;
+			Risk = currentPosition.Risk + addRisk;
+		}
+	}
+}
